5dbe84fbca3658d0d9262e8ffdbd695cbc86007f
PaulaViveiros <paulaviveiros@yahoo.com.br>
2026-04-30 13:04:18 -0300

    Update server message from 'PGalo' to 'Galo'

--- Semestre-02/Desenvolvimento-Web/app.py
+++ Semestre-02/Desenvolvimento-Web/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>PGalo Chef: Servidor Ativo!</h1>"
+    return "<h1>Galo Chef: Servidor Ativo!</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
